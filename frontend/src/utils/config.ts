18cc0b97c7affb3724b791bb0d3b013f49986ce4
Abdayemco <wassim.abdayem@gmail.com>
2025-08-22 23:05:29 +0300

    Create config.ts

---
+++ frontend/src/utils/config.ts
@@ -0,0 +1,6 @@
+// src/utils/config.ts
+
+export const API_URL = import.meta.env.VITE_API_URL;
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+export const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+export const TIMEZONEDB_API_KEY = import.meta.env.VITE_TIMEZONEDB_API_KEY;
