780d20ad3ca6533092adb7b03cdf326bf00b4301
Abdayemco <wassim.abdayem@gmail.com>
2025-11-06 22:36:47 +0200

    JOB1234

---
+++ backend/prisma/migrations/20251106203608_rename_future_to_job/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - The values [PRIVATE_TRAINER,FUTURE1,FUTURE2,FUTURE3,FUTURE4] on the enum `Role` will be removed. If these variants are still used in the database, this will fail.
+  - The values [PRIVATE_TRAINER,FUTURE1,FUTURE2,FUTURE3,FUTURE4] on the enum `VehicleType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Role_new" AS ENUM ('ADMIN', 'DRIVER', 'CUSTOMER', 'SHOPPER', 'HAIR_DRESSER', 'INSTITUTE', 'CLEANER', 'GARDENER', 'PLUMBER', 'ELECTRICIAN', 'ERRAND_RUNNER', 'BABYSITTER', 'TUTOR', 'LAUNDRY', 'PAINTER', 'PHYSIOTHERAPIST', 'ELDER_CARE', 'JOB1', 'JOB2', 'JOB3', 'JOB4');
+ALTER TABLE "User" ALTER COLUMN "role" TYPE "Role_new" USING ("role"::text::"Role_new");
+ALTER TYPE "Role" RENAME TO "Role_old";
+ALTER TYPE "Role_new" RENAME TO "Role";
+DROP TYPE "Role_old";
+COMMIT;
+
+-- AlterEnum
+BEGIN;
+CREATE TYPE "VehicleType_new" AS ENUM ('CAR', 'TUKTUK', 'DELIVERY', 'LIMO', 'WHEELCHAIR', 'TRUCK', 'WATER_TRUCK', 'TOW_TRUCK', 'SHOPPER', 'HAIR_DRESSER', 'CLEANER', 'GARDENER', 'INSTITUTE', 'BEAUTY', 'PLUMBER', 'ELECTRICIAN', 'ERRAND_RUNNER', 'BABYSITTER', 'TUTOR', 'LAUNDRY', 'PAINTER', 'PHYSIOTHERAPIST', 'ELDER_CARE', 'JOB1', 'JOB2', 'JOB3', 'JOB4');
+ALTER TABLE "User" ALTER COLUMN "vehicleType" TYPE "VehicleType_new" USING ("vehicleType"::text::"VehicleType_new");
+ALTER TABLE "Ride" ALTER COLUMN "vehicleType" TYPE "VehicleType_new" USING ("vehicleType"::text::"VehicleType_new");
+ALTER TABLE "Vehicle" ALTER COLUMN "type" TYPE "VehicleType_new" USING ("type"::text::"VehicleType_new");
+ALTER TYPE "VehicleType" RENAME TO "VehicleType_old";
+ALTER TYPE "VehicleType_new" RENAME TO "VehicleType";
+DROP TYPE "VehicleType_old";
+COMMIT;
