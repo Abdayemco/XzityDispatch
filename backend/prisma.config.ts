d8735bf32377fe1ea8dc57fc15731ac6863bf3c3
Abdayemco <wassim.abdayem@gmail.com>
2025-12-25 21:01:28 +0200

    Update prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,11 +1,8 @@
-// prisma.config.ts
-import { defineConfig } from '@prisma/internals';
-
-export default defineConfig({
+export default {
   schema: './prisma/schema.prisma',
   datasource: {
     db: {
-      adapter: process.env.DATABASE_URL, // expects url in env var
+      adapter: process.env.DATABASE_URL,
     },
   },
-});
+};
